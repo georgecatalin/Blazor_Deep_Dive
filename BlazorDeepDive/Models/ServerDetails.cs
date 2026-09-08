3cf3111a8d23139f26d08f33fac3bd957c3ed54c
George Calin <george.calin@gmail.com>
2026-09-08 09:17:58 +0300

    Implicit Razor Expressions

---
+++ BlazorDeepDive/Models/ServerDetails.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorDeepDive.Models
+{
+    public class ServerDetails
+    {
+        public ServerDetails()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 1);
+            isOnline = randomNumber == 0 ? false : true;
+        }
+
+
+        public int Id { get; set; }
+        public bool isOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}
